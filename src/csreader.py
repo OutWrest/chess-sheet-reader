8d91c2c48e772637cc6916683bf8a9b64c81ce97
Salah <47069801+OutWrest@users.noreply.github.com>
2021-01-15 21:06:33 -0600

    Updated code

--- src/csreader.py
+++ src/csreader.py
@@ -1,11 +1,13 @@
 import numpy as np
 from csimage import CSImage
 import cv2
+import math
 import pytesseract
 
 class CSReader:
     def __init__(self, img: np.ndarray) -> None:
         self.CSImg = CSImage(img)
+        self.THICKNESS_C = 500000
 
         # Moves dict, move id to move str
         self.bestGuess = {} 
@@ -35,14 +37,38 @@ class CSReader:
         gray = cv2.cvtColor(tableImg, cv2.COLOR_BGR2GRAY)
         _, thresh = cv2.threshold(gray, 250, 2**8 - 1, cv2.THRESH_BINARY_INV)
 
-        horizontal_kernel = cv2.getStructuringElement(cv2.MORPH_RECT, (width//2,1))
-        detect_horizontal = cv2.morphologyEx(thresh, cv2.MORPH_OPEN, horizontal_kernel, iterations=2)
-        cnts = cv2.findContours(detect_horizontal, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
-        cnts = cnts[0] if len(cnts) == 2 else cnts[1]
-        for c in cnts:
-            cv2.drawContours(tableImg, [c], -1, (36,255,12), 2)
+        Hmask = np.zeros((width, height, 3), np.uint8)
+        Hmask[:] = (255, 255, 255)
 
-        showImg(tableImg)
+        horizontal_kernel = cv2.getStructuringElement(cv2.MORPH_RECT, (width//4, 1))
+        detect_horizontal = cv2.morphologyEx(thresh, cv2.MORPH_OPEN, horizontal_kernel, iterations = 2)
+        cnts, _ = cv2.findContours(detect_horizontal, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+
+        for c in cnts[:44]: # 44 h lines
+            area = cv2.contourArea(c)
+            if area > (height//45//15)*width:
+                cv2.drawContours(Hmask, [c], -1, (0, 0, 0), (width*height)//self.THICKNESS_C)
+
+        Vmask = np.zeros((width, height, 3), np.uint8)
+        Vmask[:] = (255, 255, 255)
+
+        vertical_kernel = cv2.getStructuringElement(cv2.MORPH_RECT, (1, height//4 ))
+        detect_vertical = cv2.morphologyEx(thresh, cv2.MORPH_OPEN, vertical_kernel, iterations = 2)
+        cnts, _ = cv2.findContours(detect_vertical, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+
+        for c in cnts[:2]: # 2 v lines (in the middle)
+            area = cv2.contourArea(c)
+            if area > (height//45//15)*width:
+                cv2.drawContours(Vmask, [c], -1, (0, 0, 0), (width*height)//self.THICKNESS_C)
+
+        pt_img = cv2.bitwise_or(Hmask, Vmask)
+
+        pt_img = cv2.cvtColor(pt_img, cv2.COLOR_BGR2GRAY)
+
+        cv2.drawContours(pt_img, cnts, -1, (0, 0, 255), 3)
+
+        showImg(pt_img)
+
 
 
 if __name__ == "__main__":
